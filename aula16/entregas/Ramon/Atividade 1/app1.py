57f866bfe46680c6f4b2b7fb1235c39cbe8e056c
ramon <ramon.servio@gmail.com>
2025-04-30 20:25:36 -0300

    entregas

--- aula16/entregas/Ramon/Atividade 1/app1.py
+++ aula16/entregas/Ramon/Atividade 1/app1.py
@@ -1,3 +1,14 @@
+"""
+****  SEM USAR IA ****
+
+Crie um programa que:
+1. Peça dois números ao usuário
+2. Divida o primeiro pelo segundo
+3. Trate divisão por zero com mensagem clara
+4. Trate entradas inválidas com mensagem específica
+"""
+
+
 def dividir_numeros():
     try:
         num1 = float(input("Digite o primeiro número: "))
