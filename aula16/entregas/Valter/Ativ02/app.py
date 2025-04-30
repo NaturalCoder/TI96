2041372f93e432da3e4a22ec6062569a42e26b31
Valter <valteranddre@gmail.com>
2025-04-30 20:01:47 -0300

    entrega

--- aula16/entregas/Valter/Ativ02/app.py
+++ aula16/entregas/Valter/Ativ02/app.py
@@ -7,11 +7,20 @@ Crie um gerenciador de contexto simplificado:
 3. Use finally para garantir o fechamento do arquivo
 """
 
+
 def ler_arquivo(nome_arquivo):
     arquivo = None
-    arquivo = open(nome_arquivo, 'r')
-    print(arquivo.read())
-    arquivo.close()
-    print("Arquivo fechado com sucesso.")
+    try:
+        arquivo = open(nome_arquivo, 'r')
+        print(arquivo.read())
+    except FileNotFoundError:
+        print(f"Erro: O arquivo '{nome_arquivo}' não foi encontrado.")
+    finally:
+        if arquivo:
+           arquivo.close()
+        print("Arquivo fechado com sucesso.")
 
-ler_arquivo("dadosXX.txt") # Erro proposital de digitação (dados.txt)
+
+ler_arquivo("dadosXX.txt")  
+
+
